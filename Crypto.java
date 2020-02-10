d984429c22ec44e5db5f88608e83a62c1bbc0658
Yasker <mateusz_fido@wp.pl>
2020-02-10 12:28:39 +0100

    A Java program letting the user encrypt a message.
    Encrypts a message using the Ceasar cypher method with a possibility to decrypt it.

--- Crypto.java
+++ Crypto.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 class Crypto {
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please provide a string for encryption: ");
